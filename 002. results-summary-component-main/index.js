66900bdb44d0eb13524087152a649c53ea02ad93
gutoPsilva <60670589+gutoPsilva@users.noreply.github.com>
2023-04-13 10:30:48 -0300

    fix

--- 002. results-summary-component-main/index.js
+++ 002. results-summary-component-main/index.js
@@ -8,7 +8,7 @@ let m = 92;
 let e = 61;
 let i = 72;
 
-
+/*
 function calculate(){
     overallStats = Math.round((r + m + e + i) / 4);
     overallDoc.innerHTML = overallStats;
@@ -60,5 +60,4 @@ visualDoc.onclick = function(){
     visualDoc.innerHTML = `${Number(i)} <span class="maxStats">/ 100</span>`;
     calculate();
 }
-
-
+*/
