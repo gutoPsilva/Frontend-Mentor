fa94e0b86bc5191168104386a22f8e1df5906b8c
Gustavo Pereira da Silva <60670589+gutoPsilva@users.noreply.github.com>
2023-04-18 09:56:24 -0300

    Update index.js

--- 002. results-summary-component-main/index.js
+++ 002. results-summary-component-main/index.js
@@ -13,49 +13,49 @@ function calculate(){
     overallStats = Math.round((r + m + e + i) / 4);
     overallDoc.innerHTML = overallStats;
     if(overallDoc.innerHTML == "NaN"){
-        window.alert("Você digitou texto ao invés de número! ERRADO!")
+        window.alert("You typed text instead of numbers! WRONG!")
     }
 }
 
 overallDoc.onclick = function(){
-    window.alert("Caso queira ter um overall diferente, clique nas pontuações abaixo e altere-as.");
+    window.alert("If you want to change the overall, click on the scores and change them!");
 }
 
 reactionDoc.onclick = function(){
-    r = Number(window.prompt("Digite a nova pontuação: "));
+    r = Number(window.prompt("Type the new score: "));
     while(r<0 || r>100){
-        window.alert("A pontuação deve ser um número de 0 a 100!")
-        r = window.prompt("Digite a nova pontuação: ");
+        window.alert("The score needs to be a number between 0 and 100!")
+        r = window.prompt("Type the new score: ");
     }
     reactionDoc.innerHTML = `${Number(r)} <span class="maxStats">/ 100</span>`;
     calculate();
 }
 
 memoryDoc.onclick = function(){
-    m = Number(window.prompt("Digite a nova pontuação: "));
+    m = Number(window.prompt("Type the new score: "));
     while(m<0 || m>100){
-        window.alert("A pontuação deve ir de 0 a 100!")
-        m = window.prompt("Digite a nova pontuação: ");
+        window.alert("The score needs to be a number between 0 and 100!")
+        m = window.prompt("Type the new score: ");
     }
     memoryDoc.innerHTML = `${Number(m)} <span class="maxStats">/ 100</span>`;
     calculate();
 }
 
 verbalDoc.onclick = function(){
-    e = Number(window.prompt("Digite a nova pontuação: "));
+    e = Number(window.prompt("Type the new score: "));
     while(e<0 || e>100){
-        window.alert("A pontuação deve ir de 0 a 100!");
-        e = Number(window.prompt("Digite a nova pontuação: "));
+        window.alert("The score needs to be a number between 0 and 100!")
+        e = Number(window.prompt("Type the new score: "));
     }
     verbalDoc.innerHTML = `${Number(e)} <span class="maxStats">/ 100</span>`;
     calculate();
 }
 
 visualDoc.onclick = function(){
-    i = Number(window.prompt("Digite a nova pontuação: "));
+    i = Number(window.prompt("Type the new score: "));
     while(i<0 || i>100){
-        window.alert("A pontuação deve ir de 0 a 100!");
-        i = Number(window.prompt("Digite a nova pontuação: "));
+        window.alert("The score needs to be a number between 0 and 100!")
+        i = Number(window.prompt("Type the new score: "));
     }
     visualDoc.innerHTML = `${Number(i)} <span class="maxStats">/ 100</span>`;
     calculate();
