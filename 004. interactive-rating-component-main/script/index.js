d4e9a09fd42a9f5ab1b9ddac264fe1883e7b6c14
gutoPsilva <60670589+gutoPsilva@users.noreply.github.com>
2023-04-28 12:27:59 -0300

    disabled/enabled button js

--- 004. interactive-rating-component-main/script/index.js
+++ 004. interactive-rating-component-main/script/index.js
@@ -1,9 +1,12 @@
+const submitButton = document.getElementById("submit")
 const b1 = document.getElementById("b1");
 const b2 = document.getElementById("b2");
 const b3 = document.getElementById("b3");
 const b4 = document.getElementById("b4");
 const b5 = document.getElementById("b5");
 let number;
+
+submitButton.disabled = true; // desativa o botão
 
 b1.onclick = function(){
   number = 1;
@@ -41,10 +44,11 @@ function unselectAll(){
   b3.className = "rating-button";
   b4.className = "rating-button";
   b5.className = "rating-button";
+  submitButton.disabled = false; // ativa o botão apos selecionar
 }
 
-document.getElementById("submit").onclick = function(){
-  if(b1.className == "selected" || b2.className == "selected" || b3.className == "selected" || b4.className == "selected" || b5.className == "selected"){
+submitButton.onclick = function(){
+  if(submitButton.disabled == false){ // se o botão estiver ativado, avança
     sessionStorage.setItem("number", number);
     window.location.href = "indexthanks.html";
   }
