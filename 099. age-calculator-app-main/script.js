940a0c4d907d42f0601153b750bdf44f4bc8ff7b
gutoPsilva <60670589+gutoPsilva@users.noreply.github.com>
2023-04-30 01:56:06 -0300

    90%

--- 099. age-calculator-app-main/script.js
+++ 099. age-calculator-app-main/script.js
@@ -4,6 +4,12 @@ const monthAlert = document.getElementById("mAlert");
 const dayAlert = document.getElementById("dAlert");
 
 calculateButton.onclick = function(){
+  validadeData();
+}
+
+
+
+function validadeData(){
   const dataAtual = new Date();
   let inputDay = Number(document.getElementById("inputDay").value);
   let inputMonth = Number(document.getElementById("inputMonth").value);
@@ -18,14 +24,14 @@ calculateButton.onclick = function(){
   else if(inputDay < 1 || inputDay > 31 ){ // dias vão de 1 até 31
     dayAlert.innerHTML = "Must be a valid day"
   }
+  else if(inputMonth == 2 && inputDay == 31 || inputMonth == 4 && inputDay == 31 || inputMonth == 6 && inputDay == 31 || inputMonth == 9 && inputDay == 31 || inputMonth == 11 && inputDay == 31){ // meses que não possuem 31 dias
+    dayAlert.innerHTML = "Must be a valid date1";
+  }
   else if(inputDay > 29 && inputMonth == 2){ // fev não passa de 29 dias
     dayAlert.innerHTML = "Must be a valid date";
   }
   else if(inputDay == 29 && inputMonth == 2 && inputYear % 4 != 0){ // 29 fev mas não é ano bissexto 
     dayAlert.innerHTML = "Must be a valid date";
-  }
-  else if(inputMonth == 2 || inputMonth == 4 || inputMonth == 6 || inputMonth == 9 || inputMonth == 11 && inputDay == 31){ // meses que não possuem 31 dias
-    dayAlert.innerHTML = "Must be a valid date1";
   }
   else{
     dayAlert.innerHTML = "";
@@ -56,14 +62,20 @@ calculateButton.onclick = function(){
     anoValido = true;
   }
 
+  // dados válidos
   if(diaValido && mesValido && anoValido){
-    let years = inputYear - Number(dataAtual.getFullYear());
-    window.alert(years);
+    let years = Number(dataAtual.getFullYear()) - inputYear;
+    let months = (Number(dataAtual.getMonth()) + 1) - inputMonth; // getMonth retorna de 0 a 11, portanto +1 para inteirar os 12
+    let days = Number(dataAtual.getDate()) - inputDay;
+    document.getElementById("resultYear").innerHTML = Math.abs(years);
+    document.getElementById("resultMonth").innerHTML = Math.abs(months);
+    document.getElementById("resultDay").innerHTML = Math.abs(days);
   }
-}
-
-function validadeData(){
-  
+  else{
+    document.getElementById("resultYear").innerHTML = "- -";
+    document.getElementById("resultMonth").innerHTML = "- -";
+    document.getElementById("resultDay").innerHTML = "- -";
+  }
 }
 
 
