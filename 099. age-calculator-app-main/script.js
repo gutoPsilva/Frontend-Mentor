c52b66606279a35e3f54432de24a49617c4bb2ab
gutoPsilva <60670589+gutoPsilva@users.noreply.github.com>
2023-04-30 02:24:22 -0300

    sum01

--- 099. age-calculator-app-main/script.js
+++ 099. age-calculator-app-main/script.js
@@ -1,13 +1,11 @@
-const calculateButton = document.getElementById("advance");
 const yearAlert = document.getElementById("yAlert");
 const monthAlert = document.getElementById("mAlert");
 const dayAlert = document.getElementById("dAlert");
 
-calculateButton.onclick = function(){
+
+document.getElementById("advance").onclick = function(){
   validadeData();
 }
-
-
 
 function validadeData(){
   const dataAtual = new Date();
@@ -20,45 +18,60 @@ function validadeData(){
 
   if(document.getElementById("inputDay").value === ""){ // input vazio
     dayAlert.innerHTML = "This field is required";
+    styleDay();
   }
   else if(inputDay < 1 || inputDay > 31 ){ // dias vão de 1 até 31
-    dayAlert.innerHTML = "Must be a valid day"
+    dayAlert.innerHTML = "Must be a valid day";
+    styleDay();
   }
   else if(inputMonth == 2 && inputDay == 31 || inputMonth == 4 && inputDay == 31 || inputMonth == 6 && inputDay == 31 || inputMonth == 9 && inputDay == 31 || inputMonth == 11 && inputDay == 31){ // meses que não possuem 31 dias
-    dayAlert.innerHTML = "Must be a valid date1";
+    dayAlert.innerHTML = "Must be a valid date";
+    styleDay();
   }
   else if(inputDay > 29 && inputMonth == 2){ // fev não passa de 29 dias
     dayAlert.innerHTML = "Must be a valid date";
+    styleDay();
   }
   else if(inputDay == 29 && inputMonth == 2 && inputYear % 4 != 0){ // 29 fev mas não é ano bissexto 
     dayAlert.innerHTML = "Must be a valid date";
+    styleDay();
   }
   else{
     dayAlert.innerHTML = "";
+    document.getElementById("labelDay").classList.remove("label-wrongStyle");
+    document.getElementById("inputDay").classList.remove("input-wrongStyle");
     diaValido = true;
   }
 
 
   if(document.getElementById("inputMonth").value === ""){ // input vazio
     monthAlert.innerHTML = "This field is required";
+    styleMonth();
   }
   else if(inputMonth < 1 || inputMonth > 12){ // meses vão de 1 até 12
     monthAlert.innerHTML = "Must be a valid month";
+    styleMonth();
   }
   else{
     monthAlert.innerHTML = "";
+    document.getElementById("labelMonth").classList.remove("label-wrongStyle");
+    document.getElementById("inputMonth").classList.remove("input-wrongStyle");
     mesValido = true;
   }
 
 
   if(document.getElementById("inputYear").value === ""){ // input vazio
     yearAlert.innerHTML = "This field is required";
+    styleYear();
   }
   else if(inputYear > dataAtual.getFullYear()){ // input de ano a frente do ano atual
     yearAlert.innerHTML = "Must be in the past";
+    styleYear();
   }
   else{
     yearAlert.innerHTML = "";
+    document.getElementById("labelYear").classList.remove("label-wrongStyle");
+    document.getElementById("inputYear").classList.remove("input-wrongStyle");
     anoValido = true;
   }
 
@@ -78,9 +91,17 @@ function validadeData(){
   }
 }
 
+function styleDay(){
+  document.getElementById("labelDay").className = "label-wrongStyle";
+  document.getElementById("inputDay").className = "input-wrongStyle";
+}
 
+function styleMonth(){
+  document.getElementById("labelMonth").className = "label-wrongStyle";
+  document.getElementById("inputMonth").className = "input-wrongStyle";
+}
 
-// function invalidDayStyles(){
-//   document.getElementById("labelDay").style.color = "var(--LightRed)";
-//   document.getElementById("inputDay").style.border = "1 px solid var(--lightRed)";
-// }
+function styleYear(){
+  document.getElementById("labelYear").className = "label-wrongStyle";
+  document.getElementById("inputYear").className = "input-wrongStyle"
+}
