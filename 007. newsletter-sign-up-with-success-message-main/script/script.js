d0b252570a983aba83ec5673e603320dfab89a99
gutoPsilva <60670589+gutoPsilva@users.noreply.github.com>
2023-05-29 15:32:02 -0300

    95% done

--- 007. newsletter-sign-up-with-success-message-main/script/script.js
+++ 007. newsletter-sign-up-with-success-message-main/script/script.js
@@ -1,8 +1,26 @@
 const formElement = document.getElementById("form");
 formElement.addEventListener('submit', (event) => {
   event.preventDefault();
-  const emailAdress = document.getElementById("email").value;
-  console.log(emailAdress.includes('@' && '.'));
-  const pattern = / @./
-  console.log(pattern.test(emailAdress));
+  
+
+  const inputEmail = document.getElementById("email");
+  const emailAdress = inputEmail.value;
+  const alertMsg = document.querySelector('.wrong-msg');
+
+  if(!emailAdress.includes('@' && '.')){
+    alertMsg.style.opacity = 1;
+    inputEmail.classList.add('wrong-email');
+    return;
+  }
+  else{
+    alertMsg.style.opacity = 0;
+    inputEmail.classList.remove('wrong-email');
+    document.querySelector(".sign-up-form").style.opacity = 0;
+    setInterval(() => {
+      document.querySelector(".sign-up-form").style.display = 'none';
+      document.querySelector(".sucess-message").style.display = 'flex';
+    }, 500);
+    setInterval(() => document.querySelector(".sucess-message").style.opacity = 1, 800);
+    document.getElementById('js-email').innerHTML = emailAdress;
+  }
 });
