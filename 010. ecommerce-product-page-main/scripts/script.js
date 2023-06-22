743d93eeecdd1abfe2651419ffe25b5abbf96cd5
gutoPsilva <60670589+gutoPsilva@users.noreply.github.com>
2023-06-22 17:57:39 -0300

    menu done

--- 010. ecommerce-product-page-main/scripts/script.js
+++ 010. ecommerce-product-page-main/scripts/script.js
@@ -1,6 +1,9 @@
 const cartQuantityElement = document.querySelector(".quantity");
 const addQuantityButton = document.querySelector(".icon-plus");
 const removeQuantityButton = document.querySelector(".icon-minus");
+const menuElement = document.querySelector(".menu-icon");
+const closeMenuElement = document.querySelector(".icon-close");
+const menuOverlay = document.querySelector('.menu-overlay');
 
 let cartQuantity = JSON.parse(localStorage.getItem("cartQnt")) || 0;
 
@@ -23,4 +26,18 @@ removeQuantityButton.addEventListener('click', () => {
   }else{
     alert("Cart quantity must be atleast 0!");
   }
+});
+
+menuElement.addEventListener('click', () => {
+  menuOverlay.style.display = 'block';
+  setTimeout(() => {
+    menuOverlay.style.opacity = '1';
+  }, 1);
+});
+
+closeMenuElement.addEventListener('click', () => {
+  menuOverlay.style.opacity = '0';
+  setTimeout(() => {
+    menuOverlay.style.display = 'none';
+  }, 350); // .35s transition
 });
