bbef7f31d0351f115da368ac68b817907a772608
gutoPsilva <60670589+gutoPsilva@users.noreply.github.com>
2023-07-30 21:21:24 -0300

    challenge complete

--- 014. product-preview-card-component-main/tailwind.config.js
+++ 014. product-preview-card-component-main/tailwind.config.js
@@ -7,12 +7,20 @@ module.exports = {
         fraunces: "'Fraunces', serif",
         montserrat: "Montserrat, sans-serif",
       },
+      fontSize: {
+        '4.5xl': '2.5rem' 
+      }, 
       colors: {
         darkGrayishBlue: "hsl(228, 12%, 48%)",
         veryDarkBlue: "hsl(212, 21%, 14%)",
         cream: "hsl(30, 38%, 92%)",
         darkCyan: "hsl(158, 36%, 37%)",
+        darkerCyan: "#1A4031",
       },
+      width: {
+        '55%' : '55%',
+        '45%' : '45%'
+      }
     },
   },
   plugins: [],
