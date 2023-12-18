69cc9b08bd34333b9609237c572dd541e7c1fc33
gutoPsilva <60670589+gutoPsilva@users.noreply.github.com>
2023-12-17 22:22:50 -0300

    side-bar-mobile

---
+++ 021. multi-step-form-main/src/app/side-bar-mobile/side-bar-mobile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-bar-mobile',
+  standalone: true,
+  imports: [],
+  templateUrl: './side-bar-mobile.component.html',
+  styleUrl: './side-bar-mobile.component.css'
+})
+export class SideBarMobileComponent {
+
+}
