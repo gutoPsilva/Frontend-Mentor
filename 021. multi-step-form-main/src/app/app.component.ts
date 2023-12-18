b71a7ac2cfeabe532a4ff2b31edffe0a70393d90
gutoPsilva <60670589+gutoPsilva@users.noreply.github.com>
2023-12-18 00:31:36 -0300

    main container desktop & mobile transition w/ nav sidebars

--- 021. multi-step-form-main/src/app/app.component.ts
+++ 021. multi-step-form-main/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { SideBarMobileComponent } from './side-bar-mobile/side-bar-mobile.component';
+import { SideBarDesktopComponent } from './side-bar-desktop/side-bar-desktop.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SideBarMobileComponent],
+  imports: [CommonModule, RouterOutlet, SideBarMobileComponent, SideBarDesktopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ 021. multi-step-form-main/src/app/side-bar-desktop/side-bar-desktop.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-bar-desktop',
+  standalone: true,
+  imports: [],
+  templateUrl: './side-bar-desktop.component.html',
+  styleUrl: './side-bar-desktop.component.css'
+})
+export class SideBarDesktopComponent {
+
+}
