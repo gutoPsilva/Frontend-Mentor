a3d039ecad1c6150c1d9b7fbfd7bd633fa9baadc
gutoPsilva <gustavo.pds0303@gmail.com>
2024-07-28 21:56:11 -0300

    feat: layout inicial mobile completo

--- 024. mortgage-repayment-calculator-main/tailwind.config.ts
+++ 024. mortgage-repayment-calculator-main/tailwind.config.ts
@@ -13,6 +13,20 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        'jakarta': ['"Plus Jakarta Sans"', 'sans-serif'],
+      },
+      colors: {
+        "lime-fe": "hsl(61, 70%, 52%)",
+        "red-fe": "hsl(4, 69%, 50%)",
+        slate: {
+          100: 'hsl(202, 86%, 94%)',
+          300: 'hsl(203, 41%, 72%)',
+          500: 'hsl(200, 26%, 54%)',
+          700: 'hsl(200, 24%, 40%)',
+          900: 'hsl(202, 55%, 16%)',
+        },
+      }
     },
   },
   plugins: [],
