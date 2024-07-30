7d3dd33078614ea8b2c8ab32f6f1aa6908b9892d
gutoPsilva <gustavo.pds0303@gmail.com>
2024-07-30 19:25:07 -0300

    feat: implementação lógica dos cálculos de repayment e tratativas de valores

--- 024. mortgage-repayment-calculator-main/app/page.tsx
+++ 024. mortgage-repayment-calculator-main/app/page.tsx
@@ -15,20 +15,51 @@ export default function Home() {
     type: 0,
   });
 
-  const handleChange = (prop: keyof typeof mortgage, value: string | number | undefined ) => {
-    if(!value && value != 0 ) return;
+  const [repayment, setRepayment] = useState({
+    monthly: 0,
+    total: 0
+  });
 
+  const handleChange = (prop: keyof typeof mortgage, value: string | number | undefined ) => {
+    if(value == undefined) value = 0;
+
+    if(!value && value != 0) return;
+  
     setMortgage((prevMortgage) => ({
       ...prevMortgage,
       [prop]: +value,
     }));
-
-    console.log(mortgage)
   };
 
+  const calculateRepayment = () => {
+    const { amount, mortgage_term, interest_rate, type } = mortgage;
+    let repayment = 0;
+
+    const principal = amount;
+    const monthlyInterestRate = interest_rate / 12 / 100;
+    const numberOfPayments = mortgage_term * 12;
+
+    if (monthlyInterestRate > 0) {
+      repayment = (principal * monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfPayments)) / (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
+    } else {
+      repayment = principal / numberOfPayments;
+    }
+
+    const totalOverTerm = repayment * numberOfPayments;
+
+    setRepayment({
+      monthly: repayment,
+      total: totalOverTerm
+    });
+  };
+
+
+
   return (
+
     <main className="flex flex-col min-h-screen w-full font-jakarta bg-white md:flex-row md:w-fit md:min-h-min md:h-min md:rounded-3xl md:overflow-hidden md:mx-4">
-      <article className="flex flex-col w-full p-4 mb-3 md:max-w-md md:mb-0 md:p-8">
+
+      <article className="flex flex-col w-full p-4 mb-3 md:max-w-lg md:mb-0 md:p-8">
 
         <section className="w-full flex flex-col transition items-start gap-2 md:flex-row md:items-end md:justify-between">
           <h1 className="font-bold text-2xl text-slate-900">Mortgage Calculator</h1>
@@ -45,6 +76,9 @@ export default function Home() {
               id="mortgage-amount"
               name="mortgage-amount"
               decimalsLimit={2}
+              decimalSeparator="."
+              groupSeparator=","
+              onEmptied={() => handleChange("amount", 0)}
               className="p-3 flex-grow"
               onValueChange={(value) => handleChange("amount", value)}
             />
@@ -80,6 +114,8 @@ export default function Home() {
                 name="interest-rate"
                 decimalsLimit={2}
                 className="p-3 w-full"
+                decimalSeparator="."
+                groupSeparator=","
                 onValueChange={(value) => handleChange("interest_rate", value)}
               />
               <div className="flex min-h-full px-5 items-center justify-center bg-slate-100">
@@ -104,21 +140,34 @@ export default function Home() {
           </div>
         </section>
 
-        <button className="text-slate-900 bg-lime-fe-900 font-bold flex gap-2 justify-center items-center mt-4 rounded-3xl p-3 transition hover:bg-lime-fe-300 md:w-fit md:px-10 md:mt-8">
+        <button onClick={calculateRepayment} className="text-slate-900 bg-lime-fe-900 font-bold flex gap-2 justify-center items-center mt-4 rounded-3xl p-3 transition hover:bg-lime-fe-300 md:w-fit md:px-10 md:mt-8">
           <Image src={CalculatorSVG} alt="Ilustração Calculadora" className="w-fit h-fit" />
           <span>Calculate Repayments</span>
         </button>
 
       </article>
 
-      <article className="bg-slate-900 flex flex-col px-4 py-8 items-center justify-center md:max-w-md md:rounded-bl-9xl md:w-fit md:px-14">
-        <Image src={IlustrationSVG} alt="Empty Ilustration"></Image>
+      <article className="bg-slate-900 flex flex-col px-4 py-8 items-center justify-center md:max-w-lg md:rounded-bl-9xl md:w-fit md:px-14">
+        { 
+          repayment.monthly == 0 ? 
+          (
+            <>
+              <Image src={IlustrationSVG} alt="Empty Ilustration"></Image>
 
-        <h2 className="text-white font-bold text-2xl mt-2">Results shown here</h2>
+              <h2 className="text-white font-bold text-2xl mt-2">Results shown here</h2>
 
-        <p className="text-slate-300 text-center mt-4 md:max-w-96">
-          Complete the form and click “calculate repayments” to see what your monthly repayments would be.
-        </p>
+              <p className="text-slate-300 text-center mt-4 md:max-w-96">
+                Complete the form and click “calculate repayments” to see what your monthly repayments would be.
+              </p>
+            </>
+          ) :
+          (
+            <>
+              <h2 className="text-white font-bold text-2xl">Your results</h2>
+              <p className="text-slate-300 text-justify mt-4">Your results are shown below based on the information you provided. To adjust the results, edit the form and click “calculate repayments” again.</p>
+            </>
+          )
+        }
       </article>
     </main>
   );
